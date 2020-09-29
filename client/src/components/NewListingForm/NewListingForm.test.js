8eab0ff5af9337c74302065723074687a0f55996
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-09-28 21:56:10 -0500

    changed the userFunctions file from jsx to js

--- client/src/components/NewListingForm/NewListingForm.test.js
+++ client/src/components/NewListingForm/NewListingForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import ListingForm from "./ListingForm";
+import "../userFunctions";
 
 const setup = () => {
   const utils = render(<ListingForm />);
