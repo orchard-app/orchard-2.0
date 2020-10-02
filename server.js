2b3b4f8e881af7569e58aa59c1434da6a55c38b2
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-10-01 19:32:01 -0500

    uncommented out server code

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   // app.use('/api/categories', require('./controllers/Categories'));
-//   app.use("/api/items", require("./controllers/Items"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client/build/"));
-//   });
-// } else {
-//   app.use(express.static(path.join(__dirname, '/client/public')));
-//   app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
-//   });
+  app.use(express.static("client/build"));
+  // app.use('/api/categories', require('./controllers/Categories'));
+  app.use("/api/items", require("./controllers/Items"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/"));
+  });
+} else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
 }
 // Add routes, both API and view
 //app.use(routes);
