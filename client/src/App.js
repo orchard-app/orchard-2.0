884395094dce87d8454273d3296655f6d502292e
Steve Munoz <stevomunoz@yahoo.com>
2020-10-03 16:12:34 -0500

    fixed merged conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,44 @@
+// import React from "react";
+// import { Route, Switch } from "react-router-dom";
+// import { Container } from "react-bootstrap";
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// import { NavBar, Footer, Loading, PrivateRoute } from "./components";
+// import { Home, Profile, ExternalApi } from "./views";
+// // import View from './components/Pages/View';
+// import About from "./components/Pages/About";
+// import ListingFormPage from "./components/Pages/ListingFormPage";
+// import Add from "./components/Pages/Add";
+// import Viewlistings from "./components/Pages/Viewlistings";
+
+// import "./App.css";
+
+// const App = () => {
+//   const { isLoading } = useAuth0();
+
+//   if (isLoading) {
+//     return <Loading />;
+//   }
+
+//   return (
+//     <div className="everything">
+//       <Navbar />
+//       <Router>
+//         <Route exact path="/" component={Form} />
+//         <Route exact path="/menu" component={OrchardContainer} />
+//         {/* <Route exact path="/items" component={View} /> */}
+//         <Route exact path="/viewlistings" component={Viewlistings} />
+//         <Route exact path="/about" component={About} />
+//         <Route exact path="/menu#NewListing" component={ListingFormPage} />
+//         <Route exact path="/menu#Add" component={Add} />
+//       </Router>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
@@ -9,7 +50,6 @@ import { Home, Profile, ExternalApi } from "./views";
 import About from "./components/Pages/About";
 import ListingFormPage from "./components/Pages/ListingFormPage";
 import Add from "./components/Pages/Add";
-import Viewlistings from "./components/Pages/Viewlistings";
 
 import "./App.css";
 
@@ -21,19 +61,6 @@ const App = () => {
   }
 
   return (
-<<<<<<< HEAD
-    <div className="everything">
-      <Navbar />
-      <Router>
-        <Route exact path="/" component={Form} />
-        <Route exact path="/menu" component={OrchardContainer} />
-        {/* <Route exact path="/items" component={View} /> */}
-        <Route exact path="/viewlistings" component={Viewlistings}/>
-        <Route exact path="/about" component={About} />
-        <Route exact path="/menu#NewListing" component={ListingFormPage} />
-        <Route exact path="/menu#Add" component={Add} />
-      </Router>
-=======
     <div id="app" className="d-flex flex-column h-100">
       <NavBar />
       <Container className="flex-grow-1 mt-5">
@@ -47,7 +74,6 @@ const App = () => {
           <PrivateRoute path="/external-api" component={ExternalApi} />
         </Switch>
       </Container>
->>>>>>> master
       <Footer />
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-<<<<<<< HEAD
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
-=======
 import { BrowserRouter as Router } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth0-provider-with-history";
->>>>>>> master
 
 import "./index.css";
 
