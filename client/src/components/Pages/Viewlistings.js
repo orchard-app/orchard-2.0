5f481fecb3ff2906f36aac11d8f6a0ef55cc662e
Adrian Palacios <adrian.palacios0403@gmail.com>
2020-10-06 12:51:11 -0500

    view listing changed

--- client/src/components/Pages/Viewlistings.js
+++ client/src/components/Pages/Viewlistings.js
@@ -3,29 +3,29 @@ import { listItems } from "../userFunctions";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 export default class Viewlistings extends Component {
-  state = { listItemThis: [] };
+	state = { listItemThis: [] };
 
-  componentDidMount() {
-    this.getThisFunctionRunning();
-  }
+	componentDidMount() {
+		this.getThisFunctionRunning();
+	}
 
-  getThisFunctionRunning = () => {
-    listItems()
-      .then((res) => {
-        console.log(res);
-        this.setState({ listItemThis: res });
-      })
-      .catch((err) => console.error(err));
-  };
+	getThisFunctionRunning = () => {
+		listItems()
+			.then((res) => {
+				console.log(res);
+				this.setState({ listItemThis: res });
+			})
+			.catch((err) => console.error(err));
+	};
 
-  render() {
-    return (
-      <div>
-        <Container>
-          <Row>
-            {this.state.listItemThis.map((item) => (
-              <Col key={item.id}>
-                <Card className="text-center">
+	render() {
+		return (
+			<div>
+				<Container>
+					<Row>
+						{this.state.listItemThis.map((item) => (
+							<Col key={item.id} xs={12} sm={12} md={6} lg={4}>
+								{/* <Card className="text-center">
                   <Card.Header>Starts: {item.start_date}</Card.Header>
                   <Card.Body>
                     <Card.Title>{item.title}</Card.Title>
@@ -34,12 +34,45 @@ export default class Viewlistings extends Component {
                   <Card.Footer className="text-muted">
                     Ends: {item.end_date}
                   </Card.Footer>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+                </Card> */}
+								<Card className="text-center" style={backgroundStyle}>
+									<Card.Header style={titleStyle}>{item.title}</Card.Header>
+									<Card.Body style={bodyStyle}>
+										<Card.Title></Card.Title>
+										<div>
+											<strong>Address</strong>: {item.address}
+										</div>
+										<div>
+											<strong>City</strong>: {item.city}
+										</div>
+										<div>
+											<strong>State</strong>: {item.state}
+										</div>
+										<div>
+											<strong>ZipCode</strong>: {item.zipcode}
+										</div>
+										<div>
+											<strong>Start Date</strong>: {item.start_date}
+										</div>
+										<div>
+											<strong>End Date</strong>: {item.end_date}
+										</div>
+										<div>
+											<strong>Start Time</strong>: {item.start_time}
+										</div>
+										<div>
+											<strong>End Time</strong>: {item.end_time}
+										</div>
+										<div>
+											<strong>Details</strong>: {item.details}
+										</div>
+									</Card.Body>
+								</Card>
+							</Col>
+						))}
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
