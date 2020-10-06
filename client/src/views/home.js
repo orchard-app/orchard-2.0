174a70434f23f55a2826efe59be5868920ea3efc
Steve Munoz <stevomunoz@yahoo.com>
2020-10-05 19:54:46 -0500

    added picture of Steve to homepage

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
+import Steve from "../components/Steve";
 
 import { Hero, Content } from "../components";
 
 const Home = () => (
   <Fragment>
     <Hero />
+    <Steve />
   </Fragment>
 );
 
